388282c2f80a01b661dc200a71e8eefe81156c70
almightymoon <atharqulimoon@gmail.com>
2024-05-15 14:39:51 +0500

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: `/github-pages-test`,
+  base: `github-pages-test`,
   server: {
     headers: {
       "Access-Control-Allow-Origin": "*",
