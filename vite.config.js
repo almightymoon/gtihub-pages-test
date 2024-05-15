e4743a89d757c02c9e17cb6b31ac89f0b8f7d700
almightymoon <atharqulimoon@gmail.com>
2024-05-15 14:07:17 +0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: `/github-pages-test`,
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  },
+});
